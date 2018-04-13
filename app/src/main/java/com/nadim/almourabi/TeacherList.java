19d63142761ae25ecc7e217b2c3977e25ae2c7e0
Nadim <nadimouertani@hotmail.com>
2018-04-13 12:40:32 +0100

    TeacherList a class with get && set method to get and set all the teacher data.

---
+++ app/src/main/java/com/nadim/almourabi/TeacherList.java
@@ -0,0 +1,43 @@
+package com.nadim.almourabi;
+
+/**
+ * Created by nadim on 4/13/18.
+ * from tunisia with love
+ */
+public class TeacherList {
+    private String FirstName,LastName,Subject;
+
+    public TeacherList(){
+
+    }
+
+    public TeacherList(String FirstName, String LastName, String Subject){
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Subject = Subject;
+    }
+
+    public String getFirstName(){
+        return FirstName;
+    }
+
+    public void setFirstName(String x){
+        this.FirstName = x;
+    }
+
+    public String getLastName(){
+        return LastName;
+    }
+
+    public void setLastName(String x){
+        this.LastName = x;
+    }
+
+    public String getSubject(){
+        return Subject;
+    }
+
+    public void setSubject(String x){
+        this.Subject = x;
+    }
+}
