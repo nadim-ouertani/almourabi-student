9042537b871be88f4dc67fc162efdd5f5b7df32e
Nadim <nadimouertani@hotmail.com>
2018-04-16 19:01:47 +0100

    Add a custom viewPager to desable the swapabel touch event for the user.

---
+++ app/src/main/java/com/nadim/almourabi/customViewPager.java
@@ -0,0 +1,31 @@
+package com.nadim.almourabi;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by nadim on 4/16/18.
+ * from tunisia with love
+ */
+public class customViewPager extends ViewPager {
+
+    public customViewPager(Context context) {
+        super(context);
+    }
+
+    public customViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev){
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev){
+        return false;
+    }
+}
