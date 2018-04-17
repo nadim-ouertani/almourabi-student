32c77048f9f701cfb723afb34238707d904d32ea
Nadim <nadimouertani@hotmail.com>
2018-04-17 15:05:18 +0100

    Remove all animation and work as simple as possible.

--- app/src/main/java/com/nadim/almourabi/MainActivity.java
+++ app/src/main/java/com/nadim/almourabi/MainActivity.java
@@ -1,8 +1,11 @@
 package com.nadim.almourabi;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
     //Fragments
-
     Home home;
     Notification notification;
     Profile profile;
@@ -32,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //Hide action bar
         getSupportActionBar().hide();
 
@@ -40,7 +43,12 @@ public class MainActivity extends AppCompatActivity {
 
         //Initializing viewPager
         viewPager = findViewById(R.id.pagerAdapter);
+
+        //Set custom animation for the viewpager
+
+        //Dont refresh data from databse every time we swap between views
         viewPager.setOffscreenPageLimit((3) - 1);
+
         //Initializing the bottomNavigationView
         bottomNavigationView = findViewById(R.id.navigationBottom);
 
@@ -48,15 +56,17 @@ public class MainActivity extends AppCompatActivity {
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        FragmentTransaction ft;
                         switch (item.getItemId()) {
                             case R.id.navigation_home:
-                                viewPager.setCurrentItem(0);
+                                viewPager.setCurrentItem(0, false);
                                 break;
                             case R.id.navigation_notifications:
-                                viewPager.setCurrentItem(1);
+                                viewPager.setCurrentItem(1, false);
                                 break;
                             case R.id.navigation_profile:
-                                viewPager.setCurrentItem(2);
+                                viewPager.setCurrentItem(2, false);
                                 break;
                         }
                         return false;
@@ -73,12 +83,10 @@ public class MainActivity extends AppCompatActivity {
             public void onPageSelected(int position) {
                 if (prevMenuItem != null) {
                     prevMenuItem.setChecked(false);
-                }
-                else
-                {
+                } else {
                     bottomNavigationView.getMenu().getItem(0).setChecked(false);
                 }
-                Log.d("page", "Page selected: "+position);
+                Log.d("page", "Page selected: " + position);
                 bottomNavigationView.getMenu().getItem(position).setChecked(true);
                 prevMenuItem = bottomNavigationView.getMenu().getItem(position);
 
@@ -97,9 +105,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         pagerAdapter adapter = new pagerAdapter(getSupportFragmentManager());
-        home=new Home();
-        notification=new Notification();
-        profile=new Profile();
+        home = new Home();
+        notification = new Notification();
+        profile = new Profile();
         adapter.addFragment(home);
         adapter.addFragment(notification);
         adapter.addFragment(profile);
@@ -111,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         if (mAuth.getCurrentUser() == null) {
-            startActivity(new Intent(MainActivity.this,login.class));
+            startActivity(new Intent(MainActivity.this, login.class));
             finish();
         }
     }
