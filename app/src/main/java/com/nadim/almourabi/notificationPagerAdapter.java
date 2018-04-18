34a6355d705cc4d83c77ee8c54d7226e543f15eb
Nadim <nadimouertani@hotmail.com>
2018-04-18 15:54:13 +0100

    Add pagerAdapter for our tablayout.

---
+++ app/src/main/java/com/nadim/almourabi/notificationPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.nadim.almourabi;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by nadim on 4/18/18.
+ * from tunisia with love
+ */
+public class notificationPagerAdapter extends FragmentPagerAdapter {
+    private final static int PAGE = 2;
+
+    notificationPagerAdapter(FragmentManager fm) {
+        super(fm);
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new Todo();
+            case 1:
+                return new Evaluation();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Home work";
+            case 1:
+                return "Eva";
+            default:
+                return null;
+        }
+    }
+}
