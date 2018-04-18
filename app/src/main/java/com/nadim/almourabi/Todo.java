9c45add2b2adb41023ae7799c8bef77acd0c369d
Nadim <nadimouertani@hotmail.com>
2018-04-18 15:54:49 +0100

    Change the view thing...

--- app/src/main/java/com/nadim/almourabi/Todo.java
+++ app/src/main/java/com/nadim/almourabi/Todo.java
@@ -6,7 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.nadim.almourabi.R;
 
 
@@ -14,7 +21,6 @@ import com.nadim.almourabi.R;
  * A simple {@link Fragment} subclass.
  */
 public class Todo extends Fragment {
-
 
     public Todo() {
         // Required empty public constructor
@@ -25,7 +31,9 @@ public class Todo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo, container, false);
+        View view = inflater.inflate(R.layout.fragment_todo, container, false);
+
+        return view;
     }
 
 }
