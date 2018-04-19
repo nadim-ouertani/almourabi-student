d6440054c51a78d5657eb3bf05683fd3f78792f9
Nadim <nadimouertani@hotmail.com>
2018-04-19 14:55:51 +0100

    Remove method from the class.

--- app/src/main/java/com/nadim/almourabi/Teacher.java
+++ app/src/main/java/com/nadim/almourabi/Teacher.java
@@ -1,8 +1,5 @@
 package com.nadim.almourabi;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 /**
  * Created by nadim on 3/25/18.
  * from tunisia with love
@@ -21,11 +18,5 @@ public class Teacher {
         this.firstname = firstname;
         this.lastname = lastname;
         this.LGS = LGS;
-    }
-
-    void writeNewUser(String userId, String firstname, String lastname, String LGS) {
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-        Teacher teacher = new Teacher(firstname, lastname, LGS);
-        ref.child("teachers").child(userId).setValue(teacher);
     }
 }
