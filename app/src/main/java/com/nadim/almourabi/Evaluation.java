9080a76d036f5e2fd9272be7f94318b572b33898
Nadim <nadimouertani@hotmail.com>
2018-04-19 14:53:40 +0100

    Reformat code.

--- app/src/main/java/com/nadim/almourabi/Evaluation.java
+++ app/src/main/java/com/nadim/almourabi/Evaluation.java
@@ -3,10 +3,12 @@ package com.nadim.almourabi;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -15,14 +17,18 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Evaluation extends Fragment {
+    private List<EvaList> evaLists = new ArrayList<>();
+    private EvaListAdapter mAdapter;
 
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    TextView notification;
     private FirebaseAuth mAuth;
 
     public Evaluation() {
@@ -36,19 +42,47 @@ public class Evaluation extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_evaluation, container, false);
 
-        DatabaseReference ref = database.getReference();
-        mAuth = FirebaseAuth.getInstance();
-        notification = view.findViewById(R.id.notification);
-        ref.child("eva").addChildEventListener(new ChildEventListener() {
+        //RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.evaRecyclerView);
+        mAdapter = new EvaListAdapter(evaLists);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext()) {
+        };
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
+        //Set the adapter to the recyclerView
+        recyclerView.setAdapter(mAdapter);
+
+        //Get instance from the database and the Auth
+        DatabaseReference ref = database.getReference();
+        final DatabaseReference myRef = database.getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        //Point the ref var on the EVA child to fetch eva
+        ref.child("eva").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                Eva eva = dataSnapshot.getValue(Eva.class);
-                assert eva != null;
+
+                //Get the eva class
+                final Eva eva = dataSnapshot.getValue(Eva.class);
+
+                //Search the eva tha have the same student id
                 if (eva.Sid.equals(mAuth.getCurrentUser().getUid())) {
-                    //String key = dataSnapshot.getKey();
-                    notification.setText("Your eva: " + eva.Eval + " Sub: " + eva.Sub + " From teacher: " + eva.Tid + " Your id: " + eva.Sid);
-                    System.out.println("Notification found!");
+
+                    //Set the eval from the CHAR
+                    switch (eva.Eval) {
+                        case "B":
+                            eva.Eval = "BAD";
+                            break;
+                        case "G":
+                            eva.Eval = "GOOD";
+                            break;
+                        default:
+                            eva.Eval = "No EVAL";
+                    }
+
+                    //Bind the view with the recyclerVIew
+                    prepareEvaData("firstName", "lastName", eva.Sub, eva.Eval);
                 }
             }
 
@@ -76,4 +110,13 @@ public class Evaluation extends Fragment {
         return view;
     }
 
+    private void prepareEvaData(String firstName, String lastName, String Subject, String Eva) {
+        EvaList evaList = new EvaList(firstName, lastName, Subject, Eva);
+        evaLists.add(evaList);
+
+        //Notify the list that the data has been changed.
+        mAdapter.notifyDataSetChanged();
+    }
+
 }
+
