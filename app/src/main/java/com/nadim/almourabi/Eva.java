b9231bdf208f0ff2132de8d463212eb0986c5213
Nadim <nadimouertani@hotmail.com>
2018-04-19 14:51:32 +0100

    Reformat code and remove method from Eva class.

--- app/src/main/java/com/nadim/almourabi/Eva.java
+++ app/src/main/java/com/nadim/almourabi/Eva.java
@@ -1,8 +1,5 @@
 package com.nadim.almourabi;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 /**
  * Created by nadim on 4/2/18.
  * from tunisia with love
@@ -20,11 +17,5 @@ public class Eva {
         this.Sid = Sid;
         this.Eval = Eval;
         this.Sub = Sub;
-    }
-
-    void writeNewEva(String Tid, String Sid, String Eval, String Sub) {
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-        ref.child("eva").push().setValue(new Eva(Tid, Sid, Eval, Sub));
-
     }
 }
