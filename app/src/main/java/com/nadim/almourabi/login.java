9d74da58f63a648c8b61065f6a19d6973aa91f0b
Nadim <nadimouertani@hotmail.com>
2018-04-19 14:55:10 +0100

    Reformat code.

--- app/src/main/java/com/nadim/almourabi/login.java
+++ app/src/main/java/com/nadim/almourabi/login.java
@@ -1,28 +1,23 @@
 package com.nadim.almourabi;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
-
-    private static final String TAG = "Login";
     private String e, p;
     private EditText email, password;
     private FirebaseAuth mAuth;
+
     //public static final String status = "STUDENT";
 
     @Override
@@ -48,12 +43,10 @@ public class login extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "signInWithEmail:success");
                                     startActivity(new Intent(login.this, MainActivity.class));
                                     finish();
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
                                 }
 
                                 // ...
@@ -75,7 +68,6 @@ public class login extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        //password.setGravity(Gravity.RIGHT);
         // Check if user is signed in (non-null) and update UI accordingly.
         if (mAuth.getCurrentUser() != null) {
             startActivity(new Intent(login.this, MainActivity.class));
